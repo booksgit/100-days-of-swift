174cc830728a62e94314c6ee08fcba82adadef72
julie <47367790+booksgit@users.noreply.github.com>
2019-03-03 18:49:45 +0100

    day25

--- Project4/Project4/ViewController.swift
+++ Project4/Project4/ViewController.swift
@@ -15,6 +15,7 @@ import WebKit
 // The class we created viewC extends from UIViewC AND comforms? to WKNDelegateProtocol
 class ViewController: UIViewController, WKNavigationDelegate {
     var webView: WKWebView!
+    var progressView: UIProgressView!
 
     override func loadView() {
         // we create an instance of the class WKWebView
@@ -34,6 +35,27 @@ class ViewController: UIViewController, WKNavigationDelegate {
         
         // creating STUFF part 1
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        
+        //\\ \\ \\ \\ code implement bottomBar start
+        // .flexspace takes as much space as possible
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+        
+        
+        progressView = UIProgressView(progressViewStyle: .default)
+        progressView.sizeToFit()
+        let progressButton = UIBarButtonItem(customView: progressView)
+        
+        // spacer and refresh will appear as having errors until the line below is written
+        toolbarItems = [progressButton, spacer, refresh]
+        
+        navigationController?.isToolbarHidden = false
+        
+        //\\ \\ \\ \\ code implement bottomBar end
+        
+        // add Key Value Observer. You ll have to remove it when you are done observing
+        // you have to overrride the method observeValue (done below)
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
         
 
         
@@ -57,6 +79,8 @@ class ViewController: UIViewController, WKNavigationDelegate {
         let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
         ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
         ac.addAction(UIAlertAction(title: "hackingwithswift", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "www.youtube.com/watch?v=HluANRwPyNo", style: .default, handler: openPage))
+         ac.addAction(UIAlertAction(title: "xkcd.com", style: .default, handler: openPage))
         ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
         present(ac, animated: true)
@@ -73,6 +97,15 @@ class ViewController: UIViewController, WKNavigationDelegate {
     
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         title = webView.title
+    }
+    
+    // Key Value Observer
+    // type cast from Double to Float
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "estimatedProgress"
+        {
+            progressView.progress = Float(webView.estimatedProgress)
+        }
     }
     
 }
